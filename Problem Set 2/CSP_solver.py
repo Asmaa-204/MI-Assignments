3defc6c1b000f7c7d1fcb3e835371ed61a9ec2c7
Asmaa-204 <asmaa.abozaid204@gmail.com>
2025-11-22 17:30:50 +0200

    finised problem 2: least constraining value

--- Problem Set 2/CSP_solver.py
+++ Problem Set 2/CSP_solver.py
@@ -22,9 +22,9 @@ def one_consistency(problem: Problem) -> bool:
     return solvable
 
 # This function returns the variable that should be picked based on the MRV heuristic.
-# NOTE: We don't use the domains inside the problem, we use the ones given by the "domains" argument 
+# NOTE: We don't use the domains inside the problem, we use the ones given by the "domains" argument
 #       since they contain the current domains of unassigned variables only.
-# NOTE: If multiple variables have the same priority given the MRV heuristic, 
+# NOTE: If multiple variables have the same priority given the MRV heuristic,
 #       we order them in the same order in which they appear in "problem.variables".
 def minimum_remaining_values(problem: Problem, domains: Dict[str, set]) -> str:
     _, _, variable = min((len(domains[variable]), index, variable) for index, variable in enumerate(problem.variables) if variable in domains)
@@ -39,7 +39,7 @@ def minimum_remaining_values(problem: Problem, domains: Dict[str, set]) -> str:
 #       - If the other variable has no domain (in other words, it is already assigned), skip this constraint.
 #       - Update the other variable's domain to only include the values that satisfy the binary constraint with the assigned variable.
 #   - If any variable's domain becomes empty, return False. Otherwise, return True.
-# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument 
+# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument
 #            since they contain the current domains of unassigned variables only.
 def forward_checking(problem: Problem, assigned_variable: str, assigned_value: Any, domains: Dict[str, set]) -> bool:
     #TODO: Write this function
@@ -78,13 +78,43 @@ def forward_checking(problem: Problem, assigned_variable: str, assigned_value: A
 #   - You are not given a value for the given variable, since you should do the process for every value in the variable's
 #     domain to see how much it will restrain the neigbors domain
 #   - Here, you do not modify the given domains. But you can create and modify a copy.
-# IMPORTANT: If multiple values have the same priority given the "least restraining value" heuristic, 
+# IMPORTANT: If multiple values have the same priority given the "least restraining value" heuristic,
 #            order them in ascending order (from the lowest to the highest value).
-# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument 
+# IMPORTANT: Don't use the domains inside the problem, use and modify the ones given by the "domains" argument
 #            since they contain the current domains of unassigned variables only.
 def least_restraining_values(problem: Problem, variable_to_assign: str, domains: Dict[str, set]) -> List[Any]:
-    #TODO: Write this function
-    NotImplemented()
+    # TODO: Write this function
+    # only consider binary constraints involving the assigned variable
+    relatet_constraints = [
+        c
+        for c in problem.constraints
+        if isinstance(c, BinaryConstraint)
+        and variable_to_assign in c.variables
+    ]
+
+    # a dict of (value, number of allowed remaining values in neighbor variables)
+    remaining_values = {}
+
+    # loop for each value in the domain of the variable to assign
+    for value in domains[variable_to_assign]:
+        # get the remaining domain elements due to this assignment
+        new_domain_len = 0
+        # how many choices remain for neighbors if we assign 'value'
+        for constraint in relatet_constraints:
+            other_var = constraint.get_other(variable_to_assign)
+            for other_val in domains[other_var]:
+                if constraint.is_satisfied(
+                    {variable_to_assign: value, other_var: other_val}
+                ):
+                    new_domain_len += 1
+        remaining_values[value] = new_domain_len
+
+    # sort values from least to most constraining
+    ordered_values = sorted(
+        remaining_values.keys(), key=lambda v: remaining_values[v], reverse=True
+    )
+
+    return ordered_values
 
 # This function should solve CSP problems using backtracking search with forward checking.
 # The variable ordering should be decided by the MRV heuristic.
